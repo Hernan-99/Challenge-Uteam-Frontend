59317d1427b66a1c80615974423d6e533c86e122
Hernan <hernan24744@gmail.com>
2025-03-15 09:16:44 -0300

    consumo de variables de entorno

--- src/utils/finalUrlGenerate.js
+++ src/utils/finalUrlGenerate.js
@@ -1,6 +1,8 @@
 import { hashedCode } from "./hash";
 
-export const finalUrl = (publicKey, privateKey) => {
+export const finalUrl = () => {
+  const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+  const privateKey = import.meta.env.VITE_PRIVATE_KEY;
   const ts = new Date().getTime();
   const hash = hashedCode(ts, privateKey, publicKey);
   const baseUrl = "https://gateway.marvel.com/v1/public/characters";
