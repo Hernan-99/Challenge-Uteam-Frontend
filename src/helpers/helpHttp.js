50d922943701d221f8221a283a2dfd692f07b482
Hernan <hernan24744@gmail.com>
2025-03-15 09:18:09 -0300

    helper para simular crud

---
+++ src/helpers/helpHttp.js
@@ -0,0 +1,95 @@
+import { finalUrl } from "../utils/finalUrlGenerate";
+let characters = [];
+
+const helpHttp = () => {
+  // CREATE
+  const post = async (character) => {
+    const path = character.thumbnailUrl.split(".").slice(0, -1).join(".");
+    const extension = character.thumbnailUrl.split(".").pop() || "jpg";
+
+    const newCharacter = {
+      id: crypto.randomUUID(),
+      name: character.name,
+      description: character.description,
+      thumbnail: {
+        path,
+        extension,
+      },
+    };
+
+    characters.push(newCharacter);
+    return newCharacter;
+  };
+
+  const getAll = async () => {
+    try {
+      const url = finalUrl();
+      console.log(url);
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error(`Error: ${res.status}`);
+      }
+      const json = await res.json();
+      characters = json?.data?.results;
+      return characters;
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+      return [];
+    }
+  };
+
+  const getById = async (id) => {
+    try {
+      const charactersFind = characters.find((el) => el.id === id);
+      if (characters) return charactersFind;
+
+      const url = finalUrl();
+      const res = await fetch(url);
+
+      if (!res.ok) {
+        throw new Error(`Error: ${res.status}`);
+      }
+
+      const data = await res.json();
+      return data?.data?.results[0] || null;
+    } catch (error) {
+      console.log("Error: ", error.message);
+      return null;
+    }
+  };
+
+  // UPDATE
+  const put = async (id, character) => {
+    const index = characters.findIndex((el) => el.id === id);
+    if (index === -1) return null;
+    const updatedCharacter = {
+      ...characters[index],
+      name: character.name,
+      description: character.description,
+      thumbnail: {
+        path: character.thumbnailUrl.split(".").slice(0, -1).join("."),
+        extension: character.thumbnailUrl.split(".").pop() || "jpg",
+      },
+    };
+
+    characters[index] = updatedCharacter;
+    return updatedCharacter;
+  };
+
+  // DELETE
+  const del = async (id) => {
+    const initialLength = characters.length;
+    characters = characters.filter((char) => char.id !== id);
+    return initialLength !== characters.length;
+  };
+
+  return {
+    post,
+    getAll,
+    getById,
+    put,
+    del,
+  };
+};
+
+export default helpHttp;
